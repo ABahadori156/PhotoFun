2e98b9915e747f65dc2bf65779649d4d9e68f4d3
Amir Bahadori <pbahadori156@gmail.com>
2016-08-06 15:08:18 -0700

    Initial Commit

---
+++ PhotoFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoFun
+//
+//  Created by Pasha Bahadori on 8/6/16.
+//  Copyright © 2016 Pelican. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoFun
+//
+//  Created by Pasha Bahadori on 8/6/16.
+//  Copyright © 2016 Pelican. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
